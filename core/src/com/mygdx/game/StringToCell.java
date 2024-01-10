af327820aac7f10447f1be4da0c53d6886f40645
DylanSponder <dylan.sponder@gmail.com>
2024-01-10 15:39:04 +1100

    Level - Create level loader - Create TileMap, TileMapRenderer and Cells
    Camera
    - Position camera to player
    - Add camera zoom functionality
    - Create resize window method
    
    Input
    - Add InputProcessor
    - Move input to update method
    
    Physics
    - Create Box2D test object

---
+++ core/src/com/mygdx/game/StringToCell.java
@@ -0,0 +1,6 @@
+package com.mygdx.game;
+
+public class StringToCell {
+}
+//for the switch case of assigning String name to Cell and Cell type initialization
+//intended to remove clutter from DungeonCrawler.Java
